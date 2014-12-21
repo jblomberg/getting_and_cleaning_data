e34a13c977a6f48bc86b313941412c3b8c6f6ead
Jesse Blomberg <jblomberg@gmail.com>
2014-12-21 06:14:08 -0800

    Initial stubs

---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+mergeDataSets <- function(dataSets)
+{
+	return(NULL)
+}
+
+extractMeasurements <- function(df)
+{
+	return(df)
+}
+
+nameActivities <- function(df)
+{
+	return(df)
+}
+
+labelVars <- function(df)
+{
+	return(df)
+}
+
+extractTidy <- function(df)
+{
+	return(df)
+}
+
